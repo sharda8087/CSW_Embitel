1f45722411ad7ffe93ef95a59e132cbf1dfca599
sharda8087 <sharma.sharda15@gmail.com>
2018-05-03 15:57:51 +0530

    second commit

--- src/main/java/PageObjects/LoginPage.java
+++ src/main/java/PageObjects/LoginPage.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -17,13 +18,16 @@ public class LoginPage extends Globals  {
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(xpath = "//*[@id='popup-newsletter-form']/div[3]/img")
+	@FindBy(xpath =".//*[@id='popup-newsletter-form']/div[3]/img") 
+	@CacheLookup
 	public WebElement closepopup;
 	
 	@FindBy(xpath = "/html/body/div[2]/header/div/div/div/div[4]/div/div[3]/span")
+	@CacheLookup 
 	WebElement clickProfileIcon;
 	
 	@FindBy(xpath = "//img[contains(@class, 'img-logo')]")
+	@CacheLookup
 	WebElement cswlogo;
 	
 	
