0f89da017101039cb2523f53084179bad04e835b
sharda8087 <sharma.sharda15@gmail.com>
2018-05-04 17:24:43 +0530

    2_commit

--- src/test/java/TestCases/LoginPageTest.java
+++ src/test/java/TestCases/LoginPageTest.java
@@ -4,7 +4,9 @@ import java.sql.Driver;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import Global.Globals;
@@ -12,12 +14,12 @@ import PageObjects.LoginPage;
 import junit.framework.Assert;
 
 public class LoginPageTest extends Globals{
+	LoginPage login = new LoginPage(driver);
 	
-	@BeforeMethod
+	@BeforeTest
 	public void launchbrowser() throws Exception  {
         openurl();
-        deletecookies();
-        
+        deletecookies();       
 		
 /* System.setProperty("browser", "Chrome");
  "D:\Embitel\chromedriver.exe"*/
@@ -25,32 +27,43 @@ public class LoginPageTest extends Globals{
 	browserNav(csw_url);*/
 	  		}
 	
-	@Test 
+	@Test(priority = 0)
 	public void closepop() {
-		LoginPage login = new LoginPage(driver);
-	login.closepopup.click();
+		login.closepopup.click();
 	
 	//PageTitle
-	String title = login.validateloginpageTitle();
+	/*String title = login.validateloginpageTitle();
 	Assert.assertEquals(title, "Shop Online for Womens Clothes, Bags, Shoes, Accessories in India - Cover Story");
+
+	boolean flag = login.validatecswImage();
+	Assert.assertTrue(flag);*/
 	}
 	
-	/*@Test(priority = 2)
+/*	@Test(priority = 1)
 	public void LoginPagetitleTest() {
 		String title = login.validateloginpageTitle();
 		Assert.assertEquals(title, "#1 Shop Online for Womens Clothes, Bags, Shoes, Accessories in India - Cover Story");
-		}*/
+		}
 	
-	/*@Test(priority = 3)
+	@Test(priority = 2)
 	public void cswlogoImageTest() {
 		boolean flag = login.validatecswImage();
 		Assert.assertTrue(flag);
 	}*/
+	
+	@Test
+	public void NormalSignin(){
+		login.minicart.click();
+	    moveToElement(login.minicart);
+	    login.minicart.click();
+	    login.clickfbsignin.click();
+	    
+	}
 
-	/*@AfterMethod
+   /* @AfterTest
 	public void TearDown() {
 		browserClose();
 	     //driver.close();
-	}*/
-	
+	}
+	*/
 }
